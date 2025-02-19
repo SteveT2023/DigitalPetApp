21c16c5dbaab6398b94d213d8c50bb0586ced8ba
Courtney Ross <cross52@student.gsu.edu>
2025-02-19 15:44:44 -0500

    problem fix

--- digitalpetapp/test/widget_test.dart
+++ digitalpetapp/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:digitalpetapp/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const DigitalPetApp());
+    await tester.pumpWidget(const SetPetNameScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
